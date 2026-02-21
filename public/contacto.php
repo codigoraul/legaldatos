3b11d20e79aa6c1b7b438bd4c8c4062144de5ef6
Raul r <prueba@correo.com>
2026-02-21 14:15:33 -0300

    Enviar formulario solo a codigoraul@gmail.com

--- public/contacto.php
+++ public/contacto.php
@@ -7,7 +7,7 @@ $SITE_URL = (isset($_SERVER['HTTP_HOST']) && is_string($_SERVER['HTTP_HOST']) &&
   ? ('https://' . $_SERVER['HTTP_HOST'])
   : 'https://www.legaldatos.cl';
 
-$TO_EMAILS_BASE = 'contacto@legaldatos.cl, codigoraul@mail.com';
+$TO_EMAILS_BASE = 'codigoraul@gmail.com';
 $TO_EMAIL = $TO_EMAILS_BASE;
 $FROM_EMAIL = 'contacto@legaldatos.cl';
 $FROM_NAME = 'LegalDatos';
